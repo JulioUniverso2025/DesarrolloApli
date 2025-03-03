c7c5a5d0eb1a4e2b5c5b810b4556e9a1c338d3e6
JulioUniverso2025 <jmalaverm2@gmail.com>
2025-03-02 21:35:50 -0500

    Se modifico el saludo en el codigo

--- Ses1.py
+++ Ses1.py
@@ -6,4 +6,4 @@ nombre=st.text_input("Por favor ingresa tu nombre:")
 
 #Mostrar el saludo si el usuario ingresa su nombre
 if nombre:
-    st.write (f"Hola {nombre} Bienvenido a est aplicacion")
+    st.write (f"Hola {nombre} Bienvenido a esta aplicacion")
