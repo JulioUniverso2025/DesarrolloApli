fb5a4736876d047e5876511fa3d7e9191663af4d
JulioUniverso2025 <jmalaverm2@gmail.com>
2025-03-02 21:58:34 -0500

    Otra modificacion interna

--- EjecucionIDL1.py
+++ EjecucionIDL1.py
@@ -1,8 +1,9 @@
 import streamlit as st
+
 st.title("Ingrese el codigo o ID del trabajador:")
 #Entrada de usuario
 valor=st.number_input()
 
-    #Ahora la ejecucion
-valor = int(input("Ingrese un número: "))
+#Ahora la ejecucion
+from FuncionesIDL1 import obtener_jefe
 obtener_jefe(df1,valor)
