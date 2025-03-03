eeea4ac2bca099827aac77157f905cab014f1d8d
JulioUniverso2025 <jmalaverm2@gmail.com>
2025-03-02 21:33:37 -0500

    Se agrego codigo a Ses1.py

---
+++ Ses1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("Hola mundo por fin me salio la sesion1")
+
+#Solicitar nombre del usuario
+nombre=st.text_input("Por favor ingresa tu nombre:")
+
+#Mostrar el saludo si el usuario ingresa su nombre
+if nombre:
+    st.write (f"Hola {nombre} Bienvenido a est aplicacion")
