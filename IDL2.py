ead625f6111e8a0df4bf2a8feb785c5bdd15b16a
JulioUniverso2025 <JMALAVERM2@GMAIL.COM>
2025-03-08 22:17:24 -0500

    Modifique no me sale el nombre del producto

--- IDL2.py
+++ IDL2.py
@@ -2,16 +2,14 @@ import streamlit as st
 st.title("Confiteria Dulcino")
 st.title("Formulario")
 
+# Solicitar el nombre del producto
+nombre = st.text_input("Ingrese el nombre del producto: ")
 #Validar nombre de producto
 def validar_nombre_producto(nombre):
     if len(nombre) < 20:
         return True
     else:
         return False
-
-
-# Solicitar el nombre del producto
-nombre = st.text_input("Ingrese el nombre del producto: ")
 if validar_nombre_producto(nombre):
     print("Nombre válido")
 else:
