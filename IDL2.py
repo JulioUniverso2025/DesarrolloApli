dcc0703896e01aac2f1c3714e2ad5e70769ffa5d
JulioUniverso2025 <JMALAVERM2@GMAIL.COM>
2025-03-08 22:27:54 -0500

    sin funciones

--- IDL2.py
+++ IDL2.py
@@ -4,21 +4,16 @@ st.title("Formulario")
 
 # Solicitar el nombre del producto
 nombre=st.text_input("Ingrese el nombre del producto: ")
-#Ingrese el precio del producto
-precio=st.text_input("Precio del producto:")
 
 #Validar nombre de producto
-def validar_nombre_producto(nombre):
+
     if len(nombre) < 20:
-        return True
+        return "Nombre valido"
     else:
-        return False
-if validar_nombre_producto(nombre):
-    print("Nombre válido")
-else:
-    print("El nombre del producto es demasiado largo")
+        return "El nombre del producto es demasiado largo"
 
-
+#Ingrese el precio del producto
+precio=st.text_input("Precio del producto:")
 
 
 
