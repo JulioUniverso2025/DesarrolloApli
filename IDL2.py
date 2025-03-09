87d3561d33dbe763a2a14784db3638229397460e
JulioUniverso2025 <JMALAVERM2@GMAIL.COM>
2025-03-08 22:04:11 -0500

    Todo dentro de IDL2

--- IDL2.py
+++ IDL2.py
@@ -2,9 +2,22 @@ import streamlit as st
 st.title("Confiteria Dulcino")
 st.title("Formulario")
 
+#Validar nombre de producto
+def validar_nombre_producto(nombre):
+    if len(nombre) < 20:
+        return True
+    else:
+        return False
+
+
 # Solicitar el nombre del producto
 nombre = st.text_input("Ingrese el nombre del producto: ")
 if validar_nombre_producto(nombre):
     print("Nombre válido.")
 else:
     print("El nombre del producto es demasiado largo.")
+
+
+
+
+
