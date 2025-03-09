99616d7a3a32c6c609ec13e7c0cc1819aac39201
JulioUniverso2025 <JMALAVERM2@GMAIL.COM>
2025-03-08 22:25:03 -0500

    me habi equivocado en tex text

--- IDL2.py
+++ IDL2.py
@@ -5,7 +5,18 @@ st.title("Formulario")
 # Solicitar el nombre del producto
 nombre=st.text_input("Ingrese el nombre del producto: ")
 #Ingrese el precio del producto
-precio=st.tex_input("Precio del producto:")
+precio=st.text_input("Precio del producto:")
+
+#Validar nombre de producto
+def validar_nombre_producto(nombre):
+    if len(nombre) < 20:
+        return True
+    else:
+        return False
+if validar_nombre_producto(nombre):
+    print("Nombre válido")
+else:
+    print("El nombre del producto es demasiado largo")
 
 
 
