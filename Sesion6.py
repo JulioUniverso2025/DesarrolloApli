d4cfe8e0530ef7eded9b86e8ab5d7ed02f3ad5e8
JulioUniverso2025 <JMALAVERM2@GMAIL.COM>
2025-03-16 18:44:34 -0500

    FALTABA UNA "B"

--- Sesion6.py
+++ Sesion6.py
@@ -5,7 +5,7 @@ import os
 SUPABASE_URL = "https://wezkgzoocxwvitbraadb.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indlemtnem9vY3h3dml0YnJhYWRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNjE1NjcsImV4cCI6MjA1NzczNzU2N30.dDmHmNGfTckeEn3zM22rBjAOTyhSujtWrmpm2tgchbg"
 
-supabase_ Client=create_client(SUPABSE_URL,SUPABASE_KEY)
+supabase_ Client=create_client(SUPABASE_URL,SUPABASE_KEY)
 
 st.title(^"Gestion de Clientes - CRYD con Supabase y Streamlit")
 #Formulario para agregar cliente
